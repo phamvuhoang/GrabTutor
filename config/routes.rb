fa253314b1f99962eb5fac9b415369e65234a115
shiftasiahenry <hoang.pham@shiftasia.com>
2016-11-07 19:13:43 +0700

    add subject

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
     resources :cities
   end
   
-  resources :users
+  resources :users do
+    resources :subjects
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
